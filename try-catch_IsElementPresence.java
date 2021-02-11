2e639f136d5ba227e77e1a4a2bbe4598c02bc409
muratersoy <muratersoy91@gmail.com>
2021-02-11 23:37:44 +0300

    try-catch with presence of any web element

---
+++ try-catch_IsElementPresence.java
@@ -0,0 +1,28 @@
+package presence;
+
+import org.testng.Assert;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class IsElementPresence{
+
+   boolean isELementPresenceText = false;
+    public void isElementPresentOnPage(String xpath) {
+        try {
+            driver.findElement(By.xpath(xpath)).getText();
+
+        } catch (java.util.NoSuchElementException e) {
+            Assert.assertFalse("the element is not here, This will succeed.",isELementPresent);
+        }
+
+    }
+    boolean isELementPresenceXpath = false;
+    public void isElementPresentOnPageXpath(String xpath) {
+        try {
+            driver.findElement(By.xpath(xpath));
+
+        } catch (java.util.NoSuchElementException e) {
+            Assert.assertFalse("the element is not here, This will succeed." ,isELementPresentXpath);
+        }
+
+    }
