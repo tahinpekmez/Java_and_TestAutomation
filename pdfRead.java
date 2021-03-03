b53352f1f81bc7b38e08cc2e9760389062dd5731
muratersoy <muratersoy91@gmail.com>
2021-03-03 10:12:29 +0300

    Add files via upload

---
+++ pdfRead.java
@@ -0,0 +1,37 @@
+	import org.apache.pdfbox.pdmodel.PDDocument;
+	import org.apache.pdfbox.text.PDFTextStripper;
+	import java.io.InputStream;
+	import java.io.BufferedInputStream;
+	import java.io.IOException;
+	import java.net.MalformedURLException;
+	import java.net.URL;
+	
+	public int getPageCount(PDDocument doc){
+        int pageCount = doc.getNumberOfPages();
+        return pageCount;
+    }
+
+    public String readPdfContent(String url) throws IOException{
+        URL pdfUrl = new URL(url);
+        InputStream in = pdfUrl.openStream();
+        BufferedInputStream bf = new BufferedInputStream(in);
+        PDDocument doc = PDDocument.load(bf);
+        int numberOfPages = getPageCount(doc);
+        String content = new PDFTextStripper().getText(doc);
+        return content;
+
+    }
+
+    public void verifyContentInPDf(String orderID) {
+        //specify the url of the pdf file
+        String url ="url adress"+orderID;
+        driver.get(url);
+        try {
+            String pdfContent = readPdfContent(url);
+            Assert.assertTrue(pdfContent.contains("Onaylandı"));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
