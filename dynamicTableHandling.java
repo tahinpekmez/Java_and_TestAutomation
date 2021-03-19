61df0743fd7a6bc4a3a43902be83d06e186200f9
muratersoy <muratersoy91@gmail.com>
2021-03-19 10:28:56 +0300

    Update dynamicTableHandling.java

--- dynamicTableHandling.java
+++ dynamicTableHandling.java
@@ -1,11 +1,11 @@
 List<WebElement> rows = driver.findElements(By.tagName("tr"));
-driver.findElement(By.xpath("//tbody/tr[" + (rows.size()-1) + "]/td/following-sibling::td/div//*[@onclick='return DeleteOnay();']")).click();
+driver.findElement(By.xpath("//tbody/tr[" + (rows.size()-1) + "]/td/following-sibling::td/div//*[@onclick='return deleteConfirm();']")).click();
 driver.switchTo().alert().accept();
         
 List<WebElement> rowsAfterDelete = driver.findElements(By.tagName("tr"));
 		
 		// Check precense of "deletedElement"
-if (driver.findElement(By.xpath("//tbody/tr["+ (rowsAfterDelete.size()-1)+"]/td/following-sibling::td[2]")).getText() != "deletedElement") {
+if (driver.findElement(By.xpath("//tbody/tr["+ (rowsAfterDelete.size()-1)+"]/td/following-sibling::td[2]")).getText() != "elementIsDeleted") {
 	Assert.assertTrue(true);
 	}
 }
