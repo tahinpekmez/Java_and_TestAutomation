d112e61b046f740d9eae3b1e172c099b9c8fefc3
muratersoy <muratersoy91@gmail.com>
2021-07-05 13:24:28 +0300

    Update precisionMethod.java

--- precisionMethod.java
+++ precisionMethod.java
@@ -1,10 +1,10 @@
-        double totalDiscountOnBasket = Precision.round(formattedCost("xpath",2,7), 2);
+        double totalDiscountOnBasket = Precision.round(formattedCost("xpath",2,7), 2); // substring method has been embedded 
         double precisionDiscountOnBasket = Precision.round(totalDiscountOnBasket, 2);
         double totalDiscount = discountCost1st + discountCost2nd + discountCost3rd;
         double precisionTotalDiscount = Precision.round(totalDiscount, 2);
 
         double totalCost = (cost1st + cost2nd + cost3rd) - totalDiscount;
-        double basketTotalCost = formattedCost("xpath", 0, 6);
+        double basketTotalCost = formattedCost("xpath", 0, 6); // substring method has been embedded 
         if(precisionDiscountOnBasket - precisionTotalDiscount <= Math.abs(0.1)){
             Assert.assertTrue(true);
         }
