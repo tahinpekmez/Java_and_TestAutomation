547420938521747a75228f4a6933db0f18aff251
muratersoy <muratersoy91@gmail.com>
2021-07-05 13:04:50 +0300

    Create precisionMethod.java

---
+++ precisionMethod.java
@@ -0,0 +1,13 @@
+        double totalDiscountOnBasket = Precision.round(formattedCost("xpath",2,7), 2);
+        double precisionDiscountOnBasket = Precision.round(totalDiscountOnBasket, 2);
+        double totalDiscount = discountCost1st + discountCost2nd + discountCost3rd;
+        double precisionTotalDiscount = Precision.round(totalDiscount, 2);
+
+        double totalCost = (cost1st + cost2nd + cost3rd) - totalDiscount;
+        double basketTotalCost = formattedCost("xpath", 0, 6);
+        if(precisionDiscountOnBasket - precisionTotalDiscount <= Math.abs(0.1)){
+            Assert.assertTrue(true);
+        }
+        if(totalCost - basketTotalCost <= Math.abs(0.1)){
+            Assert.assertTrue(true);
+        }
