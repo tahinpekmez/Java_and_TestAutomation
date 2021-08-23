a61369fb900a6d7b38fe1c38cb20e73dc444f9f6
muratersoy <muratersoy91@gmail.com>
2021-08-23 10:27:42 +0300

    Create charIteration.java

---
+++ charIteration.java
@@ -0,0 +1,14 @@
+        Actions actions = new Actions(driver);
+//        List<WebElement> text = driver.findElements(By.xpath("//*[@class='CodeMirror-code']/div"));
+        List<WebElement> text = driver.findElements(By.cssSelector(codeLine));
+
+        for(WebElement row : text){
+            sleep(1);
+            actions.click(row).build().perform();
+            sleep(1);
+            CharacterIterator characterIterator = new StringCharacterIterator(row.getText());
+            while (characterIterator.next() != CharacterIterator.DONE) {
+                Thread.sleep(20);
+                actions.sendKeys(Keys.BACK_SPACE).build().perform();
+            }
+        }
