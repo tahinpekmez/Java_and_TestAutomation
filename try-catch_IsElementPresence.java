fe871a5258367067d6dce37b5ace9bcb8405a1c4
muratersoy <muratersoy91@gmail.com>
2021-11-15 09:29:22 +0300

    Update try-catch_IsElementPresence.java

--- try-catch_IsElementPresence.java
+++ try-catch_IsElementPresence.java
@@ -4,22 +4,24 @@ import org.testng.Assert; // Just for adding the code in below, to your project
 
 public class IsElementPresence{
 
-   boolean isELementPresenceText = false;
+   boolean isELementPresenceText = true;
     public void isElementPresentOnPage(String xpath) {
         try {
             driver.findElement(By.xpath(xpath)).getText();
 
         } catch (java.util.NoSuchElementException e) {
+            isELementPresenceText = false;
             Assert.assertFalse("the element is not here, This will succeed.",isELementPresent);
         }
 
     }
-    boolean isELementPresenceXpath = false;
+    boolean isELementPresenceXpath = true;
     public void isElementPresentOnPageXpath(String xpath) {
         try {
             driver.findElement(By.xpath(xpath));
 
         } catch (java.util.NoSuchElementException e) {
+            isELementPresenceText = false;
             Assert.assertFalse("the element is not here, This will succeed." ,isELementPresentXpath);
         }
 
