42bed40c1938d26b8ead955d14a16e71164a10e5
muratersoy <muratersoy91@gmail.com>
2022-01-18 11:11:57 +0300

    Update charIteration.java

--- charIteration.java
+++ charIteration.java
@@ -1,14 +1,14 @@
-        Actions actions = new Actions(driver);
-//        List<WebElement> text = driver.findElements(By.xpath("//*[@class='CodeMirror-code']/div"));
-        List<WebElement> text = driver.findElements(By.cssSelector(codeLine));
+// char iteration on web with selenium in java
 
-        for(WebElement row : text){
-            sleep(1);
-            actions.click(row).build().perform();
-            sleep(1);
-            CharacterIterator characterIterator = new StringCharacterIterator(row.getText());
-            while (characterIterator.next() != CharacterIterator.DONE) {
-                Thread.sleep(20);
-                actions.sendKeys(Keys.BACK_SPACE).build().perform();
-            }
-        }
+Actions actions = new Actions(driver);
+List<WebElement> text = driver.findElements(By.cssSelector(codeLine));
+for (WebElement row : text) {
+    sleep(1);
+    actions.click(row).build().perform();
+    sleep(1);
+    CharacterIterator characterIterator = new StringCharacterIterator(row.getText());
+    while (characterIterator.next() != CharacterIterator.DONE) {
+        Thread.sleep(20);
+        actions.sendKeys(Keys.BACK_SPACE).build().perform();
+    }
+}
