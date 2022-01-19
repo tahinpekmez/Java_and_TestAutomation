5559829f6875fbce07debdcaa3a819ae66f55bca
muratersoy <muratersoy91@gmail.com>
2022-01-19 09:39:26 +0300

    Update readTextFile.java

--- readTextFile.java
+++ readTextFile.java
@@ -1,12 +1,15 @@
-        public void readStringTxtFileAndAddToList(String fileName) throws IOException {
-            BufferedReader bufReader = new BufferedReader(new FileReader(fileName));
-            List <String> listOfLines = new ArrayList<>();
-            String line = bufReader.readLine();
-            while (line != null) {
-                listOfLines.add(line);
-                line = bufReader.readLine();
-                for (String line : listOfLines){
-                  System.out.println(line);
-                }
+public void readStringTxtFileAndAddToList(String fileName) throws IOException {
+        BufferedReader bufReader = new BufferedReader(new FileReader(fileName));
+        List<String> listOfLines = new ArrayList<>();
+        String line = bufReader.readLine();
+
+        while (line != null) {
+            listOfLines.add(line);
+            line = bufReader.readLine();
+        }
+            for (String domain : listOfLines) {
+                sleep(100L);
+                loginAsPUser(domain);
+                keepSettingsForTheme(domain);
             }
         }
